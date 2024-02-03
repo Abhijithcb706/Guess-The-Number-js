5642cba2a8a774ae12c84eb1036efbee961825c9
Abhijith cb <abhijithcb706@gmail.com>
2024-02-03 14:36:13 +0530

    initial commit

---
+++ guess.js
@@ -0,0 +1,21 @@
+// Generate a Random Number
+let y = Math.floor(Math.random() * 10 +1);
+
+let guess = 1;
+
+
+document.getElementById("submitguess").onclick = function () {
+  console.log("y:",y);
+
+  let x = document.getElementById("guessField").value;
+
+  if (x == y) {
+    alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guess + " GUESS ");
+  } else if (x > y) {
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+  } else {
+    guess++;
+    alert("OOPS SORRY!! TRY A LARGER NUMBER");
+  }
+};
